c55ec7c375f0e01295c588c987048563805a1e57
leapar <leapar@163.com>
2017-06-13 13:53:21 +0800

    Create myconfig.php

--- config/myconfig.php
+++ config/myconfig.php
@@ -5,9 +5,9 @@ return [
     /**
      * opentsdb url
      */
-    'tsdb_search_url' => 'http://bosun.docker:4242',
+    'tsdb_search_url' => 'http://tsdbrelay.docker:4242',
     //'tsdb_search_url' => 'http://172.29.231.70:4242',
-    'tsdb_put_url' => 'http://bosun.docker:4242',
+    'tsdb_put_url' => 'http://tsdbrelay.docker:4242',
     //'tsdb_put_url' => 'http://172.29.231.177:4242',
 
     /**
