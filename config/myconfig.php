74069f79ad576faf48d19ba5f513f2490f680296
leapar <leapar@163.com>
2017-06-13 13:52:41 +0800

    Create myconfig.php

--- config/myconfig.php
+++ config/myconfig.php
@@ -5,15 +5,15 @@ return [
     /**
      * opentsdb url
      */
-    'tsdb_search_url' => 'http://192.168.1.201:4242',
+    'tsdb_search_url' => 'http://bosun.docker:4242',
     //'tsdb_search_url' => 'http://172.29.231.70:4242',
-    'tsdb_put_url' => 'http://localhost:4242',
+    'tsdb_put_url' => 'http://bosun.docker:4242',
     //'tsdb_put_url' => 'http://172.29.231.177:4242',
 
     /**
      * put tag url
      */
-    'tag_put_url' => 'http://192.168.1.200:8101',
+    'tag_put_url' => 'http://bosun.docker:8101',
     //'tag_put_url' => 'http://172.29.231.177:8101',
 
 ];
