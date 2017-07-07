c7b78689071747a6b63d0b25bd4f01b3006039e2
chengfeng(程锋.日本方正) <cheng_f@HOLD.FOUNDER.COM>
2017-07-07 13:23:38 +0800

    config

--- config/database.php
+++ config/database.php
@@ -93,6 +93,21 @@ return [
             'prefix'   => env('DB_PREFIX', ''),
         ],
 
+        //Vú”z’u
+        'mysql_vcenter'=>[
+            'driver' => 'mysql',
+            'host' => env('DB_VCENTER_HOST', 'localhost'),
+            'port' => env('DB_VCENTER_PORT', 3306),
+            'database' => env('DB_VCENTER_DATABASE', 'forge'),
+            'username' => env('DB_VCENTER_USERNAME', 'forge'),
+            'password' => env('DB_VCENTER_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
+            'prefix' => env('DB_PREFIX', ''),
+            'timezone' => env('DB_TIMEZONE', '+00:00'),
+            'strict' => env('DB_STRICT_MODE', false),
+
+        ],
     ],
 
     /*
